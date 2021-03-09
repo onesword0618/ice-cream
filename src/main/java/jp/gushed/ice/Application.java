b088b2b996c1af466e61c57c7c08499cfb4fcbef
onesword0618 <ao.akua.leo@gmail.com>
2021-03-10 00:47:54 +0900

    Add: springbootの起動確認をした
    テストの実行が確認した

---
+++ src/main/java/jp/gushed/ice/Application.java
@@ -0,0 +1,27 @@
+/**
+ * Spring Boot Execute.
+ */
+package jp.gushed.ice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Apprication Execute.
+ * 
+ * @author onesword0618
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+    /**
+     * Main.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
